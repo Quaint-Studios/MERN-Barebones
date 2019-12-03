5ffae27f3185c247ad16fbd222d575066220c34b
QuaintShanty <quaint.shanty@gmail.com>
2019-12-03 08:14:56 -0500

    Added some helper text for users getting ready for production.

--- server/lib/express/app.js
+++ server/lib/express/app.js
@@ -4,7 +4,7 @@ import PublicIP from 'public-ip';
 import greenlock from 'greenlock-express';
 import store from 'greenlock-store-fs';
 
-import { debug } from '@utils/Essentials';
+import { debug, toBe } from '@utils/Essentials';
 import { isProductionMode } from '@utils/ServerEssentials';
 
 import app from './cfg';
@@ -14,22 +14,37 @@ export default app;
 // Use the HTTPS port for production and 8182 for development.
 const PORT = process.env.DEV_PORT || 8182;
 
-const greenlockOptions = {
-  email: process.env.SSL_EMAIL, // The email address of the ACME user / hosting provider
-  agreeTos: true, // You must accept the ToS as the host which handles the certs
-  configDir: process.env.SSL_DIR, // Writable directory where certs will be saved
-  communityMember: false, // Join the community to get notified of important updates
-  telemetry: false, // Contribute telemetry data to the project
-  store,
+function greenlockCreation() {
+  // Check if the email has been set
+  if (!toBe(process.env.SSL_EMAIL) || process.env.SSL_EMAIL === '') {
+    throw new Error(`
+Set SSL_EMAIL in the .env file and be sure to configure greenlock for your SSL setup.
+You can also run the following commands to get yourself started:
+    echo -e 'example1.com\\nexample2.org' | xargs -i -n1 npx greenlock add --subject {} --altnames {},subdomain1.{},subdomain2.{}\n\n
 
-  // Using your express app:
-  // simply export it as-is, then include it here
-  app
+You can put several .tld domains at the beginning of the command and separate them with \\n.
+Or you can just put one domain. It will pipe it to the next command, npx, and replace {} with each domain name you specified, looping through them all.
+Subdomains are separated by commas, no spaces.
 
-  // , debug: true
-};
+Afterwards, run the following to agree to the terms:
+    npx greenlock defaults --subscriber-email 'ssl@example.com' --agree-to-terms`).toString();
+  }
 
-const server = isProductionMode() ? greenlock.create(greenlockOptions).listen(80, 443) : http.createServer(app).listen(PORT);
+  return greenlock
+    .init(() => ({
+      greenlock: require('./greenlock.js'),
+
+      // whether or not to run at cloudscale
+      cluster: false
+    }))
+    .ready((glx) => {
+      // Serves on 80 and 443
+      // Get's SSL certificates magically!
+      glx.serveApp(app);
+    });
+}
+
+const server = isProductionMode() ? greenlockCreation() : http.createServer(app).listen(PORT);
 
 export const listener = server;
 
